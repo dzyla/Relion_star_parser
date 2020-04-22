870cc9a346ae13b446dc152f5a0fd5315f131d92
dzyla <didny1@gmail.com>
2020-04-22 18:31:37 +0200

    Add files via upload

--- Relion_simple_star_parser.py
+++ Relion_simple_star_parser.py
@@ -9,10 +9,7 @@ Script written by Dawid Zyla, ETH Zurich.
 
 For now script allows to plot selected columns and convert Relion 3.1 star files to 3.0. Outside the CLI there is possibility to
 modify STAR file by removing columns, modifying column or filtering rows based on selected values. This however has to be done manually for now. 
-
-
 At some point there will be some features added available from command line, such as:
-
 * removing obsolete columns
 * column modification by simple arithmetic equations
 * row filtering based on the column values (e.g. _rlnNrOfSignificantSamples, _rlnMaxValueProbDistribution or just _rlnClassNumber)
@@ -20,8 +17,6 @@ At some point there will be some features added available from command line, suc
 * opening and modification of non data star files (selecting blocks, loops etc)
 * saving Relion 3.1+ data star files (or star files with multiple blocks)
 * maybe some more ideas
-
-
 '''
 
 
@@ -43,6 +38,53 @@ def save_star(dataframe_, filename='out.star'):
 
     out_doc.write_file(filename)
     print('File "{}" saved.'.format(filename))
+
+
+def save_star_31(dataframe_optics, dataframe_particles, filename='out.star'):
+    # For now only Relion star 3.1+ can be saved as 3.1 star. Adding optics will be implemented later.
+
+    out_doc = cif.Document()
+    out_particles = out_doc.add_new_block('optics', pos=-1)
+
+    # Row number is required for the column names to save the STAR file e.g. _rlnNrOfSignificantSamples #33
+    column_names = dataframe_optics.columns
+    column_names_to_star = []
+    for n, name in enumerate(column_names):
+        column_names_to_star.append(name + ' #{}'.format(n + 1))
+
+    loop = out_particles.init_loop('', column_names_to_star)
+    data_rows = dataframe_optics.to_numpy().astype(str).tolist()
+
+    # save optics loop
+    for row in data_rows:
+        loop.add_row(row)
+
+    out_particles = out_doc.add_new_block('particles', pos=-1)
+
+    column_names = dataframe_particles.columns
+    column_names_to_star = []
+    for n, name in enumerate(column_names):
+        column_names_to_star.append(name + ' #{}'.format(n + 1))
+
+    loop = out_particles.init_loop('', column_names_to_star)
+    data_rows = dataframe_particles.to_numpy().astype(str).tolist()
+
+    # save particles loop
+    for row in data_rows:
+        loop.add_row(row)
+
+    out_doc.write_file(filename)
+    print('File "{}" saved.'.format(filename))
+
+
+def convert_optics(optics_data_):
+    # used for saving Relion 3.1 files with optics groups.
+    # Changes the dict so values are list now.
+
+    for key in optics_data_.keys():
+        optics_data_[key] = [optics_data_[key]]
+
+    return optics_data_
 
 
 def convert_new_to_old(dataframe_, optics_group, filename, magnification='100000'):
@@ -206,82 +248,86 @@ def plot_columns(particles_data, col1_name, col2_name, plot_type='hist'):
 
 '''----------------------------------------------------------------------------------------------------'''
 
-import argparse
+if __name__ == '__main__':
 
-parser = argparse.ArgumentParser(description='Modify and plot Relion *.star files and convert Relion 3.1+ star to 3.0')
-parser.add_argument('--i', type=str, help='File path')
-parser.add_argument('--convert', action='store_true', default=False,
-                    help='Convert Relion 3.1+ data.star to 3.0')
-parser.add_argument('--plot', action='append', dest='plot',
-                    default=[],
-                    help='plot two columns such as: --plot _rlnMaxValueProbDistribution --plot _rlnCtfFigureOfMerit. To plot the the value against index of the particle use --plot index --plot _rlsYOURMETA')
-parser.add_argument('--plot_type', dest='plot_type',
-                    default='hist',
-                    help='Plot type: histogram (hist) (default), scatter (scat) or XY plot (line) --plot_type scat or --plot_type hist --plot_type line')
+    import argparse
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Modify and plot Relion *.star files and convert Relion 3.1+ star to 3.0')
+    parser.add_argument('--i', type=str, help='File path')
+    parser.add_argument('--convert', action='store_true', default=False,
+                        help='Convert Relion 3.1+ data.star to 3.0')
+    parser.add_argument('--plot', action='append', dest='plot',
+                        default=[],
+                        help='plot two columns such as: --plot _rlnMaxValueProbDistribution --plot _rlnCtfFigureOfMerit. To plot the the value against index of the particle use --plot index --plot _rlsYOURMETA')
+    parser.add_argument('--plot_type', dest='plot_type',
+                        default='hist',
+                        help='Plot type: histogram (hist) (default), scatter (scat) or XY plot (line) --plot_type scat or --plot_type hist --plot_type line')
 
-if not args.plot:
-    try:
-        path = args.i
-        optics_data, particles_data = parse_star(path)
-    except:
-        # Here for the run without the command line
-        path = 'run_data.star'
-        optics_data, particles_data = parse_star(path)
+    args = parser.parse_args()
 
-    print('Raw star file dimentions: {}'.format(particles_data.shape))
+    if not args.plot:
+        try:
+            path = args.i
+            optics_data, particles_data = parse_star(path)
+        except:
+            # Here for the run without the command line
+            quit()
+            path = 'run_data.star'
+            optics_data, particles_data = parse_star(path)
 
-else:
-    try:
-        path = args.i
-        optics_data, particles_data = parse_star_selected_columns(path, args.plot[0], args.plot[1])
-    except:
-        path = 'run_it025_data.star'
-        optics_data, particles_data = parse_star_selected_columns(path, args.plot[0], args.plot[1])
+        print('Raw star file dimentions: {}'.format(particles_data.shape))
 
-particles_data_ = particles_data
+    else:
+        try:
+            path = args.i
+            optics_data, particles_data = parse_star_selected_columns(path, args.plot[0], args.plot[1])
+        except:
+            quit()
+            path = 'run_it025_data.star'
+            optics_data, particles_data = parse_star_selected_columns(path, args.plot[0], args.plot[1])
 
-if args.convert:
-    convert_new_to_old(particles_data_, optics_data, args.i)
+    particles_data_ = particles_data
 
-if len(args.plot) == 2:
-    try:
-        plot_columns(particles_data, args.plot[0], args.plot[1], args.plot_type)
+    if args.convert:
+        convert_new_to_old(particles_data_, optics_data, args.i)
 
-    except KeyError:
-        print('Column not found. Check spelling or your star file')
+    if len(args.plot) == 2:
+        try:
+            plot_columns(particles_data, args.plot[0], args.plot[1], args.plot_type)
+
+        except KeyError:
+            print('Column not found. Check spelling or your star file')
+            quit()
+
+        except ValueError:
+            print('Column is a text column. Not plottable.')
+
+
+    elif len(args.plot) == 1 or len(args.plot) > 2:
+        print('Only 2 columns are accepted.')
         quit()
 
-    except ValueError:
-        print('Column is a text column. Not plottable.')
+    '''
+    Modify star columns
+    Uncomment the line if you want to change your star file, including:
+    * changing the values in the column (add, subtract, multiply or divide)
+    * select only particles (rows) which have specific values
+    * Change column names
+    * plot selected columns
+    '''
 
+    # Select rows with specific values
+    # particles_data_ = particles_data[particles_data['_rlnNrOfSignificantSamples'].astype(float) <= 10]
+    # particles_data_ = particles_data_[particles_data['_rlnMaxValueProbDistribution'].astype(float) >= 0.5]
 
-elif len(args.plot) == 1 or len(args.plot) > 2:
-    print('Only 2 columns are accepted.')
-    quit()
+    # Remove selected columns
+    # particles_data_ = particles_data_.drop(columns = ['_rlnOpticsGroup'])
 
-'''
-Modify star columns
-Uncomment the line if you want to change your star file, including:
-* changing the values in the column (add, subtract, multiply or divide)
-* select only particles (rows) which have specific values
-* Change column names
-* plot selected columns
-'''
+    # Modify values of the column
+    # particles_data_['_rlnMaxValueProbDistribution'] = particles_data_['_rlnMaxValueProbDistribution'].astype(float) * 2
 
-# Select rows with specific values
-# particles_data_ = particles_data[particles_data['_rlnNrOfSignificantSamples'].astype(float) <= 10]
-# particles_data_ = particles_data_[particles_data['_rlnMaxValueProbDistribution'].astype(float) >= 0.5]
+    # print('Modified star file dimentions: {}.\nRemoved {} particles'.format(particles_data_.shape, particles_data.shape[0]-particles_data_.shape[0]) )
 
-# Remove selected columns
-# particles_data_ = particles_data_.drop(columns = ['_rlnOpticsGroup'])
-
-# Modify values of the column
-# particles_data_['_rlnMaxValueProbDistribution'] = particles_data_['_rlnMaxValueProbDistribution'].astype(float) * 2
-
-# print('Modified star file dimentions: {}.\nRemoved {} particles'.format(particles_data_.shape, particles_data.shape[0]-particles_data_.shape[0]) )
-
-
-# Save modified file
-# save_star(particles_data_)
+    # Save modified file
+    # save_star(particles_data_)
